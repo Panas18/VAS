6ce7ed35eda283fb044be58f06da55a38dddee0d
Panas18 <panas.tiwary18@gmail.com>
2022-08-05 10:45:49 +0545

    user appointment list table view

--- src/pages/appointment/appointment.tsx
+++ src/pages/appointment/appointment.tsx
@@ -1,8 +1,66 @@
 import { Navbar } from "../../components/navbar/navbar";
 import React from "react";
-import { Button } from "antd";
+import { Button, Table } from "antd";
 import { useNavigate } from "react-router-dom";
 import "./appointment.css";
+import type { ColumnsType } from "antd/lib/table";
+
+interface DataType {
+  patientId: number;
+  siteLocation: string;
+  serviceType: string;
+  confirmationCode: string;
+}
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: "Patient ID",
+    dataIndex: "patientId",
+    key: "patientId",
+  },
+  {
+    title: "Site Location",
+    dataIndex: "siteLocation",
+    key: "siteLocation",
+  },
+  {
+    title: "Service Type",
+    dataIndex: "serviceType",
+    key: "serviceType",
+  },
+  {
+    title: "Confirmation Code",
+    dataIndex: "confirmationCode",
+    key: "confirmationCode",
+  },
+  {
+    title: "Action",
+    key: "action",
+    render: () => (
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <a href=" # ">View</a>
+        <a href=" # " style={{ color: "red" }}>
+          Delete
+        </a>
+      </div>
+    ),
+  },
+];
+
+const data: DataType[] = [
+  {
+    patientId: 1,
+    siteLocation: "Kathmandu",
+    serviceType: "Pfizer",
+    confirmationCode: "234k8",
+  },
+  {
+    patientId: 2,
+    siteLocation: "Bhaktapur",
+    serviceType: "Johnson & Johnson",
+    confirmationCode: "oius2",
+  },
+];
 
 export const AppointmentPage: React.FC = () => {
   const navigate = useNavigate();
@@ -12,10 +70,28 @@ export const AppointmentPage: React.FC = () => {
   return (
     <div>
       <Navbar />
-      <div className="addBtn">
-        <Button onClick={handleAddButton} type="primary">
-          Add Vaccine Appointment
-        </Button>
+      <div className="appointmentContainer">
+        <div className="appointment--header">
+          <div className="appointment--title">
+            <span
+              style={{
+                fontSize: "30px",
+              }}
+            >
+              Your Appointments
+            </span>
+          </div>
+          <div>
+            <Button onClick={handleAddButton} type="primary">
+              Add New Appointment
+            </Button>
+          </div>
+        </div>
+        <Table
+          columns={columns}
+          dataSource={data}
+          style={{ marginTop: "20px" }}
+        ></Table>
       </div>
     </div>
   );
